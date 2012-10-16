a20b2ab2189f8e8ab5c1893a95b30e0ca560709d
Lucas Lima de Souza <lucaslima4p@gmail.com>
2012-10-15 21:08:17 -0300

    Update src/banco/model/SaldoPositivo.java

--- src/banco/model/SaldoPositivo.java
+++ src/banco/model/SaldoPositivo.java
@@ -4,7 +4,7 @@ package banco.model;
 public class SaldoPositivo implements EstadoDeUmaConta {
 
 	@Override
-	public void saqua(Conta conta, double valor) {
+	public void saca(Conta conta, double valor) {
 		conta.saldo -=  valor;
 		if(conta.getSaldo() < 0) conta.estadoAtual = new SaldoNegativo();
 	}
